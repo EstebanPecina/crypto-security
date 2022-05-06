4ee278b5608f6753a3426c3b45be5626e6f67325
Esteban Pecina <estebanpecina14@gmail.com>
2022-05-05 21:00:35 -0500

    Initial commit

---
+++ part-1/server/controllers/auth.js
@@ -0,0 +1,42 @@
+const bcrypt = require('bcryptjs')
+const users = []
+
+module.exports = {
+    login: (req, res) => {
+      console.log('Logging In User')
+      console.log(req.body)
+      const { username, password } = req.body
+      for (let i = 0; i < users.length; i++) {
+        const existing = bcrypt.compareSync(password, users[i].pinHash)
+        if (users[i].username === username && users[i].password === password) {
+          res.status(200).send(users[i])
+          return
+        }
+      }
+      res.status(400).send("User not found.")
+    },
+    register: (req, res) => {
+        console.log('Registering User')
+        console.log(req.body)
+        users.push(req.body)
+        res.status(200).send(req.body)
+
+        const {username, email, firsName, lastName, password,} = req.body
+
+        const salt = bcrypt.genSaltSync(10)
+        const pinHash = bcrypt.hashSync(password, salt)
+
+        let passObj = {
+          username,
+          email,
+          firsName,
+          lastName,
+          pinHash
+        }
+        
+        users.push(passObj)
+        let passwordToReturn = {...passObj}
+        delete passwordToReturn.pinHash
+        res.status(200).send(passwordToReturn)
+    }
+}
